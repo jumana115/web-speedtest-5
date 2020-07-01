3a40afa85cc985058321c7126081b13231fb0f7f
jumanaalajmi115 <ku.22@hotmail.com>
2020-07-01 17:02:17 +0300

    speedtest

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -5,7 +5,12 @@ let instructor = {
     username: "careless10",
     numberOfRepos: 200,
   },
+
   sayHello: function () {
     console.log("Hi, I'm Menawer");
   },
 };
+console.log(instructor.name);
+console.log(instructor.courses[0]);
+console.log(instructor.githubAccount.username);
+console.log("Hi, I'm Menawer");
